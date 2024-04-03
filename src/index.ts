2d40467184af1e3d2bb403f6309f52f3bcfd0a41
Cristian Turcios <cturcioscolindres01@gmail.com>
2024-04-03 16:29:51 -0600

    added docker file

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ app.use(
 );
 
 // Start backend server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Backend server is running at port ${PORT}`);
 });
